5af9f9181b1fcf22fca127da0d936c42d32c498e
Bruno Machado <brunomdr46@gmail.com>
2021-08-25 20:37:49 -0300

    Add Logo and Menu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logo import logo
 import requests
 import pyotp
 import os
@@ -16,9 +17,19 @@ api_real = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_BRL&com
 usd_to_brl = api_real.json()
 preco_brl = usd_to_brl["USD_BRL"]
 
-# Checar o saldo da conta no BitSkins
-saldo = requests.get(f"https://bitskins.com/api/v1/get_account_balance/?api_key={API_KEY_BITSKINS}&code={CODE}")
-checar_saldo_usuario = saldo.json()
-saldo_usd = float(checar_saldo_usuario["data"]["available_balance"])
-saldo_usuario_brl = saldo_usd * preco_brl
-print(f"Você tem na sua conta R$ {saldo_usuario_brl:.2f}")
+# Menu do programa
+def menu():
+    print(logo)
+    print("1. Checar saldo da conta")
+    opcao_menu = input("Digite uma opção do menu:\n")
+
+    # Checar saldo da conta do BitSkins
+    if opcao_menu == "1":
+        saldo = requests.get(f"https://bitskins.com/api/v1/get_account_balance/?api_key={API_KEY_BITSKINS}&code={CODE}")
+        checar_saldo_usuario = saldo.json()
+        saldo_usd = float(checar_saldo_usuario["data"]["available_balance"])
+        saldo_usuario_brl = saldo_usd * preco_brl
+        print(f"Você tem R$ {saldo_usuario_brl:.2f} na sua conta")
+        menu()
+
+menu()
