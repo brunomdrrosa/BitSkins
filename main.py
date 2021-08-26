8c449e9625dfcb3ac4a9c4522d6490bb34298ee9
Bruno Machado <brunomdr46@gmail.com>
2021-08-26 14:21:38 -0300

    Add Buy Order

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ preco_brl = usd_to_brl["USD_BRL"]
 # Menu do programa
 def menu():
     print(logo)
+    print("=== MENU ===")
     print("1. Checar saldo da conta")
+    print("2. Checar sua última ordem de compra")
+    print("3. Fechar o programa")
     opcao_menu = input("Digite uma opção do menu:\n")
 
     # Checar saldo da conta do BitSkins
@@ -31,5 +34,17 @@ def menu():
         saldo_usuario_brl = saldo_usd * preco_brl
         print(f"Você tem R$ {saldo_usuario_brl:.2f} na sua conta")
         menu()
+
+    # Checar a última ordem de compra
+    if opcao_menu == "2":
+        ordens = requests.get(f"https://bitskins.com/api/v1/get_buy_order_history/?api_key={API_KEY_BITSKINS}&page=1&app_id=730&code={CODE}")
+        ordens_json = ordens.json()
+        nome_skin = ordens_json["data"]["orders"][0]["market_hash_name"]
+        preco_skin = ordens_json["data"]["orders"][0]["price"]
+        print(f"Sua última ordem de compra foi {nome_skin} por $ {preco_skin}")
+
+    # Sair do programa
+    if opcao_menu == "3":
+        exit()
 
 menu()
